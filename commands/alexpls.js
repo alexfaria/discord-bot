0fa48e93753d7409be1509941029562edd96ad00
Alex Faria <simaofaria11@gmail.com>
2017-04-19 23:36:12 +0100

    ayyy

--- commands/alexpls.js
+++ commands/alexpls.js
@@ -1,14 +1,14 @@
 const request = require('request');
 
 exports.run = (client, message, args) => {
-    channel.startTyping();
+    message.channel.startTyping();
     request('http://live-ur-best-life.herokuapp.com/api/quotes', (error, response, body) => {
         if (!error && response.statusCode === 200) {
             const result = JSON.parse(body);
             const n = Math.floor(Math.random() * result.length);
             const quote = result[n].quote_content;
-            channel.sendMessage(quote);
-            channel.stopTyping();
+            message.channel.sendMessage(quote);
+            message.channel.stopTyping();
         }
     });
 }
