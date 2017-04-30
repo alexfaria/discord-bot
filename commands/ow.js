ce64b4112846e959a7bff827cc7c844023d45ad7
Alex Faria <simaofaria11@gmail.com>
2017-04-30 17:51:56 +0100

    test

--- commands/ow.js
+++ commands/ow.js
@@ -2,15 +2,14 @@ const owjs = require('overwatch-js');
 
 exports.run = (client, message, args) => {
     if (!args || args.length < 1) return message.channel.sendMessage(`Must provide a username.`);
-    owjs.getAll('pc', 'eu', args[0])
-        .then((data) => {
-            console.dir(data);
-        })
     owjs.search('SadMoustache-2212')
-        .then((data) => console.dir(data, {
-            depth: 2,
-            colors: true
-        }));
+        .then((data) => {
+            console.dir(data, {
+                depth: 2,
+                colors: true
+            });
+            message.channel.sendMessage(data)
+        })
 }
 
 exports.help = "Get overwatch data.";
