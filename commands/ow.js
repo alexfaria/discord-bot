fdd28953e84a24a45b312692c52f15ed4d58390c
Alex Faria <simaofaria11@gmail.com>
2017-04-30 17:48:47 +0100

    fix typo

--- commands/ow.js
+++ commands/ow.js
@@ -1,6 +1,6 @@
 const owjs = require('overwatch-js');
 
-exports.run = (client, message, arg) => {
+exports.run = (client, message, args) => {
     if (!args || args.length < 1) return message.channel.sendMessage(`Must provide a username.`);
     owjs.getAll('pc', 'eu', args[0])
         .then((data) => {
