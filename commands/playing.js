c46e2cba5f6ed008af1895b5747b3a283a37d0d3
Alex Faria <simaofaria11@gmail.com>
2017-04-30 21:50:21 +0100

    set playing

---
+++ commands/playing.js
@@ -0,0 +1,9 @@
+exports.run = (client, message, args) => {
+    let role = message.guild.roles.get(client.config.modRoleID);
+    if (!message.member.roles.has(role.id) || message.author.id != client.config.ownerID)
+        return message.channel.sendMessage(`Only members with the role ${role} may use it.`);
+    const argument = message.content.substr(client.prefix.length + 'playing'.length);
+    client.user.setGame(argument);
+}
+
+exports.help = `Change the game I'm playing. Only members with permissions may use it.`;
