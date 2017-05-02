cb76a06b9d18edaafb778b349526f23e285a02f1
Alex Faria <simaofaria11@gmail.com>
2017-05-03 00:16:03 +0100

    check if url is present

--- commands/queue.js
+++ commands/queue.js
@@ -5,9 +5,12 @@ const youtube = `https://www.youtube.com/watch?v=`;
 exports.run = (client, message, args) => {
 
     if (!args || args.length < 1)
-        return message.channel.send(`Must provide a song name`);
+        return message.channel.send('Must provide a command: either `add <url>` or `list` ');
 
     if (args[0] === "add") {
+        if (args.length < 2)
+            return message.channel.send(`Must provide a song name`);
+
         request(endpoint + `&key=${client.config.googleAPI}&q=${args.join(' ')}`, (error, response, body) => {
             if (!error && response.statusCode === 200) {
                 const result = JSON.parse(body);
@@ -24,12 +27,12 @@ exports.run = (client, message, args) => {
                 console.dir(client.queue[message.guild]);
             }
         });
-    } else if (args[0] === list) {
+    } else if (args[0] === "list") {
         let message = "";
         for (let song in client.queue[message.guild]) {
             message = message + song + "\n";
         }
-        message.channel.send(message, {
+        message.channel.send(message, { 
             code: true
         });
     }
