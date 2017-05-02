c67aa8f3ecea27fb45bdbbf46c71eb780134e42b
Alex Faria <simaofaria11@gmail.com>
2017-05-03 00:18:37 +0100

    fix permission check

--- commands/shutdown.js
+++ commands/shutdown.js
@@ -1,6 +1,6 @@
 exports.run = (client, message, args) => {
     let role = message.guild.roles.get(client.config.modRoleID);
-    if (!message.member.roles.has(role.id) || message.author.id != client.config.ownerID)
+    if (!(message.member.roles.has(role.id) || message.author.id == client.config.ownerID))
         return message.channel.send(`Only members with the role ${role} may use it.`);
     process.exit();
 }
