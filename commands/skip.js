b05b67cbe255102394b9fe8d80d4165c2762bfdd
Alex Faria <simaofaria11@gmail.com>
2017-05-03 10:50:19 +0100

    emite the 'end' event instead of leaving the channel

--- commands/skip.js
+++ commands/skip.js
@@ -1,6 +1,6 @@
 exports.run = (client, message, args) => {
     const vcon = client.voiceConnections.get(message.guild.id);
-    if (vcon && vcon.channel) vcon.channel.leave();
+    if (vcon && vcon.channel) vcon.disconnect();
 }
 
 exports.help = "Skips the current song.";
