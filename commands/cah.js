b37451049cd2dd7f091a55f4890cc8363c7a9655
Alex Faria <simaofaria11@gmail.com>
2017-05-04 17:20:47 +0100

    cah

--- commands/cah.js
+++ commands/cah.js
@@ -3,8 +3,14 @@ const url = "http://www.crhallberg.com/cah/json/output.php";
 
 exports.run = (client, message, args) => {
     request.post(url, {
-        decks: ['Base'],
-        type: 'json'
+        form: {
+            decks: ['Base', 'CAHe1', 'CAHe2', 'CAHe3', 'CAHe4', 'CAHe5', 'CAHe6',
+                    'greenbox', '90s', 'Box', 'fantasy', 'food', 'science', 'www',
+                    'hillary', 'trumpvote', 'trumpbag', 'xmas2012', 'xmas2013', 'PAXE2013',
+                    'PAXP2014', 'PAXE2014', 'PAXP2014', 'PAXEP2014', 'PAXPP2014', 'PAX2015',
+                    'HOCAH', 'reject', 'reject2', 'Canadian', 'misprint'],
+            type: 'JSON'
+        }
     }, (error, response, body) => {
         if (!error && response.statusCode === 200) {
             const result = JSON.parse(body);
